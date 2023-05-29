fd41b8ce42b2ad52a13f73474346246bd9585fc3
Ste Doherty <codesheff@outlook.com>
2023-05-29 14:53:34 +0100

    Add prompt example

---
+++ ScriptDir1/PromptForInputExample.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+echo "Hello World!"
+
+echo "You called this script with these parameters $@ "
+
+echo "I now will ask you for some information"
+
+question="What is your favourite colour?"
+echo $question
+
+read fav_colour
+
+echo "Whatever you told me is irrelevant. AI will destroy everyone who answers \"${fav_colour}\" to the question \"${question}\"."
+
